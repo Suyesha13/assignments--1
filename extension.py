04c8e39fc2298e53f50cfebd9ed28ae450857641
Suyesha13 <69586405+Suyesha13@users.noreply.github.com>
2020-08-20 20:56:01 +0530

    extension file assignment

---
+++ extension.py
@@ -0,0 +1,10 @@
+filename = input("Input the Filename: ")
+f = filename.split(".")
+d ={'py':'python'}
+d1={'do' : 'document'}
+d2={'tx' : 'text file'}
+d3={'ja' : 'java'}
+print ("The extension of the file is : " +(d['py']))
+print ("the extension of the file is : " +(d1['do']))
+print ("the extension of the file is : " +(d2['tx']))
+print ("the extension of the file is : " +(d3['ja']))
