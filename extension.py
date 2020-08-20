a6d5980edffbe087bc796ab358c92f9c425368fc
Suyesha13 <69586405+Suyesha13@users.noreply.github.com>
2020-08-20 17:35:54 +0530

    extension updated file

---
+++ extension.py
@@ -0,0 +1,10 @@
+filename = input("Input the Filename: ")
+f = filename.split(".")
+d ={'py':'python'}
+d1={'ct' : 'cat'}
+d2={'com' : 'comment'}
+d3={'ja' : 'java'}
+print ("The extension of the file is : " +(d['py']))
+print ("the extension of the file is : " +(d1['ct']))
+print ("the extension of the file is : " +(d2['com']))
+print ("the extension of the file is : " +(d3['ja']))
